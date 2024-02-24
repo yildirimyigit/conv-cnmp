5e0e833d4d027b30716e6901497e5b95b34a8851
Mehmet Süzer <93345336+mehmetSuzer@users.noreply.github.com>
2024-02-24 15:36:00 +0300

    Add files via upload
    Implemented Convolutional CNMP
    Wrote a basic run script

---
+++ run_conv_cnmp.py
@@ -0,0 +1,23 @@
+
+import sys
+from conv_cnmp import ConvCNMP
+import torch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+                
+if len(sys.argv) != 2:
+    raise Exception("Run type (train/test) is missing")
+if sys.argv[1] != "train" and sys.argv[1] != "test":
+    raise Exception("Run type must be either train or test: %s is found" % sys.argv[1])
+else:
+    run_type = sys.argv[1]
+
+if run_type == "train":
+    train_model = ConvCNMP().to(device)
+    optimizer = torch.optim.Adam(lr=1e-4, params=train_model.parameters())
+    
+else:
+    test_model = ConvCNMP().to(device)
+
+
